59a85d0749e49af9fa15a07913148520a6f7dc03
victororiahi <156344564+victororiahi@users.noreply.github.com>
2024-05-21 19:28:56 +0100

    Added edit

--- ASPNetMVC.Web/Controllers/ProductController.cs
+++ ASPNetMVC.Web/Controllers/ProductController.cs
@@ -41,5 +41,44 @@ namespace ASPNetMVC.Web.Controllers
             return View(product);
         }
 
+        [HttpGet]
+        public ActionResult Edit(int Id)
+        {
+            var Db = new SampleDbContext();
+            var product = Db.Products.Find(Id);
+            var categories = Db.Categories.ToList();
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            product.SelectedCategoryId = product.Category==null ? 0 : product.Category.Id;
+            product.Categories = categories.Select(c => new SelectListItem { Text = c.Name, Value = c.Id.ToString() });
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product) 
+        {
+            var Db = new SampleDbContext();
+            var Dbproduct = Db.Products.Find(product.Id);
+            if (Dbproduct == null)
+            {
+                return HttpNotFound();
+            }
+            var Dbcategory = Db.Categories.Find(product.SelectedCategoryId);
+            if (Dbcategory == null)
+            {
+                return HttpNotFound();
+            }
+            Dbproduct.Name= product.Name;
+            Dbproduct.Quantity = product.Quantity;
+            Dbproduct.Color = product.Color;
+            Dbproduct.Description = product.Description;
+            Dbproduct.UnitPrice = product.UnitPrice;
+            Dbproduct.Category = Dbcategory;
+            Db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
-}
+
+    }
